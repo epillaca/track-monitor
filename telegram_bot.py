bac8e64c18a26b251212dfba6f295698016ffa70
epillaca <efrainpillaca@gmail.com>
2026-07-31 11:28:12 -0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,23 @@
 
+import os
+import requests
+
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def enviar_mensaje(texto):
+
+    if not TOKEN or not CHAT_ID:
+        print("Telegram no configurado")
+        return
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": texto
+    }
+
+    requests.post(url, data=data)

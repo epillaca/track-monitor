b6e775cee5457ba57f3bf8a94371059f0ea3a99b
epillaca <efrainpillaca@gmail.com>
2026-07-31 11:27:53 -0500

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,23 @@
 
+import json
+import os
+
+FILE = "productos.json"
+
+
+def cargar_productos():
+    if not os.path.exists(FILE):
+        return []
+
+    with open(FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def guardar_productos(productos):
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(
+            productos,
+            f,
+            ensure_ascii=False,
+            indent=2
+        )
